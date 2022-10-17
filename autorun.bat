169cce6028cc42dc469c1b6ade5f01afa1ea4fdd
Manuel <mantt89.mv@gmail.com>
2022-10-16 19:13:48 -0600

    fix autorun.bat

--- autorun.bat
+++ autorun.bat
@@ -1,2 +1,2 @@
-python -m http.server --directory "C:\Users\GPO_Modulo_2\Desktop\dist\"
-Start "C:\Program Files(x86)\Google\Chrome\Application\chrome.exe" '--start-fullscreen "localhost:8000"'
+Start python -m http.server --directory "C:\Users\GPO_Modulo_2\Desktop\dist\" 
+Start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --start-fullscreen http://localhost:1234
