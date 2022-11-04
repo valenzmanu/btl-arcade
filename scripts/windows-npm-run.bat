4fb521a52dd0b3bb8249d90d9ee25fab250694ed
Manuel <mantt89.mv@gmail.com>
2022-11-03 19:47:07 -0600

    add autorun scripts

---
+++ scripts/windows-npm-run.bat
@@ -0,0 +1,2 @@
+Start "" npm start --prefix "C:\Users\GPO_Modulo_2\Desktop\btl-arcade" 
+Start "" "C:\Program Files\Google\Chrome\Application\chrome.exe" --start-fullscreen --app=http://localhost:8000
